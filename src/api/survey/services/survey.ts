93f0ec26b32dad349ca71422465befa6f5a3f7d1
MaciAC <macia.ac@gmail.com>
2025-08-26 20:22:25 +0200

    use simpler request to get all surveys

--- src/api/survey/services/survey.ts
+++ src/api/survey/services/survey.ts
@@ -1,7 +1,30 @@
 /**
  * survey service
  */
-
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::survey.survey');
+type Params = {
+  populate?: any;
+  [key: string]: any;
+};
+
+export default factories.createCoreService('api::survey.survey', ({ strapi }) => ({
+  async find(...args) {
+    // Extract the existing arguments and modify the populate parameter
+    const params = ((args[0] ?? {}) as Params);
+
+    // Set default populate if not already specified
+    params.populate = {
+        questions: {
+            populate: {
+            answers: true
+            }
+        }
+    };
+    
+    // Call the parent find method with our modified parameters
+    const { results, pagination } = await super.find(params);
+    
+    return { results, pagination };
+  }
+}));
